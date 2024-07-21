44d0657a3faaf668a06bba4a68b536bcc36c5de0
RiyasAero <riyaspulakalhouse@gmail.com>
2024-07-21 11:27:12 +0530

    Add files via upload

---
+++ cars -list.py
@@ -0,0 +1,17 @@
+cars=['rs6', '911gtrs', 'challenger', 'bmwm4']
+
+#Add a new car to the list
+cars.append('nissan gtr')
+print("list adding a new car:",cars)
+
+#Remove a car from the list
+cars.remove('rs6')
+print("list after removing rs6:",cars)
+
+#Insert a new car to the list
+cars.insert(2,'xm')
+print("list after adding in an index value",cars)
+
+#Get the length of the list
+length_of_the_list=len(cars)
+print("length of the list :",cars)
